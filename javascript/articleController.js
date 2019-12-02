d045c2ccfb16373461469f5bd49216e96481a2f0
GuyCalloway <guyfcalloway@gmail.com>
2019-12-02 15:45:55 +0000

    built MVC

---
+++ javascript/articleController.js
@@ -0,0 +1,11 @@
+  function ArticleController(articleModel, articleView) {
+      this.model = articleModel;
+      this.view = articleView;
+  }
+
+  ArticleController.prototype.getHeadlines = function (domElement) {
+      var self = this;
+      this.model.allHeadlines(function (headlines) {
+          domElement.innerHTML = self.view.render(headlines)
+      });
+  }
---
+++ javascript/articleView.js
@@ -0,0 +1,15 @@
+  function ArticleView() {}
+
+  ArticleView.prototype.render = function (articles) {
+      return [
+          '<ul>',
+          this._renderTitles(articles),
+          '</ul>'
+      ].join('')
+  }
+
+  ArticleView.prototype._renderTitles = function (articles) {
+      return articles.map(function (article) {
+          return '<li>' + article.title + '</li>'
+      }).join('')
+  }
